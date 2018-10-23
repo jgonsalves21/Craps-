10785cc40a2219d14f210dc9f477098834dbf5af
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-10-23 13:37:27 -0400

    Add files via upload

---
+++ src/CrapsTest.java
@@ -0,0 +1,15 @@
+
+public class CrapsTest {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to the craps game!");
+		System.out.println("The rules are:");
+		System.out.println("If a 7 or an 11 is rolled  first, you win.");
+		System.out.println("If a 2 or a 12 is rolled first, you lose.");
+		System.out.println("If you roll anything else, that becomes your point");
+		System.out.println("On subsequent rolls, a 7 will make you lose, whilee getting your point is a win.");
+		System.out.println("Please press enter to roll:");
+	}
+
+}
